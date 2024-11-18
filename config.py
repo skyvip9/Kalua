d4071e0ca3a9d6e30b84de0ddef5e5d55ce31dc9
skyvip9 <sky.vip210@gmail.com>
2024-11-18 07:53:37 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY KaliaYtOwner
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7462994580:AAHd3ary2dwqD4L7646geeVeJ8ih_KBqkwY")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@KaliaYtOwner")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6862918172")
